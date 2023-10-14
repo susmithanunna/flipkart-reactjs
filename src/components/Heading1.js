ae12d3c108aa6dc0c874d7a4504d008a898f606c
susmithanunna <susmithanunna141@gmail.com>
2023-10-14 13:24:53 +0530

    mobile response1

--- src/components/Heading1.js
+++ src/components/Heading1.js
@@ -6,14 +6,14 @@ function Heading() {
   return (
     <>
       
-      <div className='headingimage col-md-2'>
-        <img src={logo1} width="160" />
+      <div className='headingimage col-md-2 col-xs-6'>
+        <img src={logo1} width="160"/>
       </div>
-      <div className='search col-md-5'>
+      <div className='search col-md-5 col-xs-12'>
         <i class="fa-solid fa-magnifying-glass"></i>
         <input class="searchbar" type="text" value="Search for Products,Brands and More" />
       </div>
-      <div class="col-md-5 user1">
+      <div class="col-md-5 .d-none .d-sm-block cuser1">
         <div class="user">
           <Link to="Become_a_Seller_form" ><i class="fa-solid fa-house"></i>Become a Seller</Link>
           <i class="fa-solid fa-user"></i>Sign in
